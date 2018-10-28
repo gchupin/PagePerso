e18243679da82c90f65dd80c579068f5631dd0a9
gchupin <guillaume.chupin@etu.u-bordeaux.fr>
2018-10-28 15:59:28 +0100

    Done carousel from tp3

--- Spa/my.js
+++ Spa/my.js
@@ -1,25 +1,64 @@
 const toiletsURL = 'http://odata.bordeaux.fr/v1/databordeaux/sigsanitaire/?format=json&callback=?';
 const kidareasURL = 'http://odata.bordeaux.fr/v1/databordeaux/airejeux/?format=json&callback=?';
-
-function dispJSONData (data)
-{
-  $("#data-list").empty ();
-  for (let i = 0; i < data.length; ++i)
-  {
-    $("#data-list").append ("<li> Nom: " + data [i].nom + "</li>");
-  }
-}
+const carouselImgURL = 'http://www.reveillere.fr/M2WEB/tds/td3/albums.json';
 
 $("#btn-toilets").on ("click", ()=> {
-  $.getJSON (toiletsURL, (result) => {
+  $.getJSON (toiletsURL, (result) => 
+  {
     //let data = JSON.parse (JSON.stringify(result))
-    dispJSONData (result.d)
+    dispJSONToillet (result.d)
   })
 });
 
 $("#btn-kidareas").on ("click", ()=> {
   $.getJSON (kidareasURL, (result) => {
     //let data = JSON.parse (JSON.stringify(result))
-    dispJSONData (result.d)
+    dispJSONKidArea (result.d)
   })
 });
+
+fetch(carouselImgURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    dispJSONImg (myJson.albums);
+    console.log(myJson.albums);
+  });
+
+function dispJSONToillet (data)
+{
+  $("#data-list").empty ();
+  for (let i = 0; i < data.length; ++i)
+  {
+    $("#data-list").append ("<li> Nom: " + data [i].nom + " | Adresse: " + data[i].adresse +  "</li>");
+  }
+}
+
+function dispJSONKidArea (data)
+{
+  $("#data-list").empty ();
+  for (let i = 0; i < data.length; ++i)
+  {
+    $("#data-list").append ("<li> Nom: " + data [i].nom + " | Age minimum: " + data[i].age_min + " | Age maximum: " + data[i].age_max + " | Nombre de jeux: " + data[i].nombre_jeux +  "</li>");
+  }
+}
+
+function dispJSONImg (data)
+{
+  $("#carousel").empty ();
+  for (let i = 0; i < data.length; ++i)
+  {
+    $("#carousel").append (" <li data-target=\"#myCarousel\" data-slide-to=" + i + "></li>");
+    if (i == 0)
+    {
+      $("#indicators").append ("<li data-target=\"#myCarousel\" data-slide-to=" + i + " class=\"active\"></li>");
+      $("#slide").append ("<div class=\"item active\"> <img src=" + data [i].url +  " alt=" + data [i].name + " style=\"margin:0px auto;\"></div>");
+      print (data [i].url);
+    }
+    if (i > 1)
+    {
+      $("#slide").append ("<div class=\"item\"> <img src=" + data [i].url +  " alt=" + data [i].name + " style=\"margin:0px auto;\">");
+    }
+  }
+}
